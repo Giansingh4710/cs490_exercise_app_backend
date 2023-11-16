ebc08cd17e9639903f73ca82ab49e55f398e3a5a
Rushi <rrpatel@gmail.com>
2023-11-16 06:44:18 -0500

    fixed db access

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2')
 require('dotenv').config()
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
   database: 'fitnessDB',
 })
 
