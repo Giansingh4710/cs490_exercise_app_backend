e882bd1df1ce84b1593050e6c37645e7358d3e1b
Rushi <rrpatel@gmail.com>
2023-11-28 12:21:30 -0500

    moved sequalize connection to database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 require("colors");
+const { Sequelize } = require("sequelize");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -8,6 +9,11 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: "fitnessDB",
 });
+
+const sequelize = new Sequelize(
+  `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/fitnessDB`
+);
+
 // call connect method to console connection infomration to the screen
 connection.connect((err) => {
   if (err) {
@@ -18,4 +24,4 @@ connection.connect((err) => {
   console.log("Connected to the database!".blue);
 });
 
-module.exports = connection;
+module.exports = {connection, sequelize};
