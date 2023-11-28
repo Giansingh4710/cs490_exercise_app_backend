c5b28493a563acc4ab3a57ab3b0f639d9be9cab3
Rushi <rrpatel@gmail.com>
2023-11-28 16:10:43 -0500

    fixed error in coaches route

--- Routes/coaches.js
+++ Routes/coaches.js
@@ -1,38 +1,43 @@
-const connection = require('../config/database')
+const { connection } = require('../config/database')
+const express = require("express");
+const router = express.Router();
 
-module.exports = (app) => {
+const { getCoachByID } = require("../Controllers/get-coach-by-id.js");
 
-    app.get('/api/coaches', (req, res) => {
-        const query = `SELECT coach.CoachID, user.firstName, user.lastName
-                      FROM coach 
-                      INNER JOIN user WHERE user.UserID = coach.CoachID 
-                      GROUP BY coach.CoachID ORDER BY coach.CoachID`
-    
-        connection.query(query, (err, results) => {
-            if(err) {
-                console.error('Error executing query:', err);
-                res.status(500).json({error: 'Error retrieving coaches from the database.'});
-                return;
-            }
-            res.json(results);
-        })
+router.get("/:CoachID", getCoachByID);
+
+router.get('/api/coaches', (req, res) => {
+    const query = `SELECT coach.CoachID, user.firstName, user.lastName
+                  FROM coach 
+                  INNER JOIN user WHERE user.UserID = coach.CoachID 
+                  GROUP BY coach.CoachID ORDER BY coach.CoachID`
+
+    connection.query(query, (err, results) => {
+        if(err) {
+            console.error('Error executing query:', err);
+            res.status(500).json({error: 'Error retrieving coaches from the database.'});
+            return;
+        }
+        res.json(results);
     })
+})
 
-    app.get('/api/coaches-search-name', (req, res) => {
-        const searchTerm = req.query.name || ''
-        const query = `SELECT coach.CoachID, user.firstName, user.lastName 
-                       FROM coach 
-                       INNER JOIN user ON user.UserID = coach.CoachID 
-                       WHERE CONCAT(user.firstName, ' ', user.lastName) LIKE '%${searchTerm}%' 
-                       GROUP BY coach.CoachID ORDER BY coach.CoachID`
-    
-        connection.query(query, (err, results) => {
-            if (err) {
-                console.error('Error executing query:', err);
-                res.status(500).json({ error: 'Error retrieving coaches from the database.' });
-                return;
-            }
-            res.json(results);
-        })
+router.get('/api/coaches-search-name', (req, res) => {
+    const searchTerm = req.query.name || ''
+    const query = `SELECT coach.CoachID, user.firstName, user.lastName 
+                   FROM coach 
+                   INNER JOIN user ON user.UserID = coach.CoachID 
+                   WHERE CONCAT(user.firstName, ' ', user.lastName) LIKE '%${searchTerm}%' 
+                   GROUP BY coach.CoachID ORDER BY coach.CoachID`
+
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Error retrieving coaches from the database.' });
+            return;
+        }
+        res.json(results);
     })
-}
+})
+
+module.exports = router
