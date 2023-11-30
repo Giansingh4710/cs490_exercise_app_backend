aa68a1d4917b935c050c2a4013dd84fe4081bfd6
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2023-11-30 18:43:00 -0500

    Delete Routes/Coaches.js
    remove error

--- Routes/Coaches.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getCoachByID } = require("../Controllers/get-coach-by-id.js");
-
-
-router.get("/:CoachID", getCoachByID);
-
-module.exports = router
