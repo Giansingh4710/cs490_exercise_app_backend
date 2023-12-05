e4ff49166411f65e15e7692dd0063564f0bfd572
Katherin Jimenez <101299564+katherinj@users.noreply.github.com>
2023-12-05 14:11:08 -0500

    updating SECRET_KEY import

--- middleware/security.js
+++ middleware/security.js
@@ -1,7 +1,7 @@
 // import jwt, error handling and security key
 const jwt = require("jsonwebtoken");
 const { UnauthorizedError } = require("../utils/errors");
-const { SECRET_KEY } = require("../sql_config/database");
+const { SECRET_KEY } = require("../sql_config/config");
 
 // extract the json token from request header
 // split the authorization header and assure that the header is Bearer and extract the token
