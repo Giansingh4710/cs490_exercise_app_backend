da766c38cfda4ac7c205998a8e575572dd1ed95b
Rushi <rrpatel@gmail.com>
2023-12-08 01:14:32 -0500

    added swagger documentation for POST /messages

--- routes/messages.js
+++ routes/messages.js
@@ -8,11 +8,28 @@ const {
 } = require("../Controllers/messages.js");
 
 /**
- * front end wont have userid, so getting from token
- * {
- *      content: "...",
- *      receiverID: "..."
- * }
+ *  @swagger
+ *  /messages:
+ *  post:
+ *      summary: Stores messages between 2 users to database
+ *      description: Stores messages between 2 users to database. It is expected one user is the coach and one is the user, although with this setup 2 users can message
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          content:
+ *                              type: string
+ *                              description: Message to be sent
+ *                              example: "Hi Coach Penn, when do you think I should increase my weight for bench press?"
+ *                          unit:
+ *                              type: receiverID
+ *                              description: integer
+ *                              example: 101
+ * 
+ * 
  */
 router.post("/", requireAuthedUser, storeMessage)
 
