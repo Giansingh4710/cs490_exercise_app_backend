db49725c7edeb3a63aedbbe441975b966b52081f
Rushi <rrpatel@gmail.com>
2023-12-09 00:17:00 -0500

    added comment for endpoint documention

--- routes/workoutPlan.js
+++ routes/workoutPlan.js
@@ -14,7 +14,7 @@ const {
  *  @swagger
  *  /workoutPlan/{userID?}:
  *  get:
- *      summary: Gets workout plan of user, userID is optional. Read description
+ *      summary: Gets workout plan of user, userID is optional. Read description. NOT FINISHED NEED UPDATES TO DATABASE. GOOD ENOUGH FOR TESTING WITH FRONTEND
  *      description: This endpoint gets the workout plan of a user. This endpoint will allow a user to view their own assigned workout plan and it will also allow a coach to view the workout plan of one of their clients
  *                  <br>GET /workoutPlan will get the signed in user's workout plan
  *                  <br>GET /workoutPlan/{userID} will allow a signed in COACH to view the workout plan of the client who's userID was passed in the path
@@ -35,6 +35,41 @@ const {
  */
 router.get("/:userID?", requireAuthedUser, getWorkoutPlan);
 
+/**
+ *  @swagger
+ *  /workoutPlan/addExercise:
+ *  get:
+ *      summary: User or Coach of User can add an exercise to their workout plan. NOT FINISHED NEED UPDATES TO DATABASE. GOOD ENOUGH FOR TESTING WITH FRONTEND
+ *      description: This endpoint gets the workout plan of a user. This endpoint will allow a user to add an exercise to their workout plan and it will also allow a coach to add an exercise the workout plan of one of their clients
+ *                  <br>
+ *                  <br>GET /workoutPlan/{userID} will allow a signed in COACH to view the workout plan of the client who's userID was passed in the path
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          userID:
+ *                              type: integer
+ *                              description: UserID of user to add exercise to workout plan for
+ *                              example: 101
+ *                          exerciseID:
+ *                              type: integer
+ *                              description: ExerciseID of exercise to be added to workout plan
+ *                              example: 12
+ *                          dayOfWeek:
+ *                              type: string
+ *                              description: Day of week to add exercise to
+ *                              example: 'monday'
+ *      responses:
+ *          201:
+ *              description: Exercise has been added to workout plan
+ *          403:
+ *              description: Coach is signed in user and is trying to access workout plan of user that is not their client. Or User is not a coach.
+ *          500:
+ *              description: Error accessing Database.
+ */
 router.post("/addExercise", requireAuthedUser, addExercise);
 
 module.exports = router;
