127cbc31255ab39e2a090d98452e066c6ecb4618
Rushi <rrpatel@gmail.com>
2023-12-10 21:27:12 -0500

    added daily survey endpoint and documention

--- dataAccess/log_activity_db.js
+++ dataAccess/log_activity_db.js
@@ -12,4 +12,52 @@ async function insertWaterIntake({ userID, intakeUnit, intakeAmount, date }) {
   return insertData;
 }
 
-module.exports = { insertWaterIntake };
+async function insertMentalState({userID, state, date}){
+    const query = "INSERT INTO MentalState(userID, state, date) VALUES (?, ?, ?)";
+    const [insertData, _] = await connection.promise().query(query, [userID, state, date])
+    return insertData;
+}
+
+// waiting on goal progress table to created in DB
+async function insertGoalProgress({userID, weight, date}){
+    const query = "INSERT INTO GoalProgress(userID, weight, date) VALUES (?, ?, ?)";
+    const [insertData, _] = await connection.promise().query(query, [userID, weight, date])
+    return insertData;
+}
+
+async function dailySurveyIsCompleted(userID, date){
+    // only checking insert of goal progress b/c daily survey is inserted using a transaction
+    const query = "SELECT * FROM GoalProgress WHERE userID = ? AND date = ?";
+    const [surveyData, _] = await connection.promise().query(query, [userID, date])
+    return surveyData.length >= 1;
+}
+
+async function insertDailySurvey(dailySurveyData, userID, date){
+    try{
+        connection.promise().beginTransaction();
+        const waterIntakeInsert = await insertWaterIntake({
+            "userID": userID,
+            intakeUnit: dailySurveyData.waterData.unit.toLowerCase(),
+            intakeAmount: dailySurveyData.waterData.amount,
+            "date": date
+        });
+        const mentalStateInsert = await insertMentalState({
+            "userID": userID,
+            state: dailySurveyData.moodData,
+            "date": date
+        })
+        const goalProgressInsert = await insertGoalProgress({
+            "userID": userID,
+            weight: dailySurveyData.weightData,
+            "date": date
+        })
+
+        connection.promise().commit();
+    }catch(error){
+        connection.promise().rollback();
+        throw new Error("Error recording daily survey");
+    }
+}
+module.exports = { 
+    insertDailySurvey, 
+    dailySurveyIsCompleted};
--- routes/LogActivity.js
+++ routes/LogActivity.js
@@ -4,10 +4,9 @@ const { requireAuthedUser, fakeAuthedUser } = require(
   "../utils/security.js",
 );
 
-const { logWaterIntake } = require("../controllers/logActivity.js");
+const { recordDailySurvey } = require("../controllers/logActivity.js");
 
 /**
- *  @swagger
  *  /logActivity/logWaterIntake:
  *  post:
  *      summary: Logs water intake recorded by user into the database
@@ -76,6 +75,89 @@ const { logWaterIntake } = require("../controllers/logActivity.js");
  *                                  type: string
  *                                  example: "Error logging water intake"                             
  */
-router.post("/logWaterIntake", requireAuthedUser, logWaterIntake);
+// router.post("/logWaterIntake", requireAuthedUser, logWaterIntake);
+
+/**
+ *  @swagger
+ *  /logActivity/recordDailySurvey:
+ *  post:
+ *      summary: Records daily survey data into database
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          waterData:
+ *                              type: object
+ *                              properties:
+ *                                  amount:
+ *                                      type: integer
+ *                                      example: 100
+ *                                  unit:
+ *                                      type: string
+ *                                      description: Unit of water
+ *                                      example: fl oz
+ *                          weightData:
+ *                              type: integer
+ *                              example: 185
+ *                          moodData:
+ *                              type: string
+ *                              example: Happy
+ *      responses:
+ *          201:
+ *              description: Daily survey recorded successfuly
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                                  example: Daily survey recorded
+ *                              details:
+ *                                  type: string
+ *                                  example: Water intake, weight, and mental state recorded
+ *          400:
+ *              description: User has already completed daily survey for the day
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              error:
+ *                                  type: object
+ *                                  properties:
+ *                                      status:
+ *                                          type: integer
+ *                                          example: 400
+ *                                      message:
+ *                                          type: string
+ *                                          example: User already completed daily survey
+ *                                      details:
+ *                                          type: string
+ *                                          example: User has already completed the daily survey for today 2023-12-10
+ *          500:
+ *              description: Error accessing database
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              error:
+ *                                  type: object
+ *                                  properties:
+ *                                      status:
+ *                                          type: integer
+ *                                          example: 500
+ *                                      message:
+ *                                          type: string
+ *                                          example: Error inserting into database
+ *                                      details:
+ *                                          type: string
+ *                                          example: Error recording daily survey
+ */
+router.post("/recordDailySurvey", requireAuthedUser, recordDailySurvey);
 
 module.exports = router;
