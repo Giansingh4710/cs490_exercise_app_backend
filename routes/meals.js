5e3ee7cc164d9a0b4ae87753614c1857202b5988
Rushi <rrpatel@gmail.com>
2023-12-10 23:33:38 -0500

    added documention for GET meals endpoint

--- routes/meals.js
+++ routes/meals.js
@@ -7,6 +7,54 @@ const {requireAuthedUser, fakeAuthedUser } = require(
 
 const { getMeals } = require("../controllers/meals.js")
 
+/**
+ *  @swagger
+ *  /meals:
+ *  get:
+ *       summary: Returns all recorded meals of user from today
+ *       responses:
+ *            200:
+ *                description: Successful response
+ *                content:
+ *                   application/json:
+ *                        example:
+ *                            breakfast:
+ *                                - foodName: Salad
+ *                                  calories: 633
+ *                            brunch:
+ *                                - foodName: Yogurt
+ *                                  calories: 1064
+ *                            lunch:
+ *                                - foodName: Pizza
+ *                                  calories: 907
+ *                            snack:
+ *                                - foodName: Steak
+ *                                  calories: 1140
+ *                                - foodName: Steak
+ *                                  calories: 275
+ *            500:
+ *                description: Error accessing Database.
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ *                            properties:
+ *                                error:
+ *                                    type: object
+ *                                    properties:
+ *                                        status:
+ *                                            type: integer
+ *                                            description: HTTP status code of response
+ *                                            example: 500
+ *                                        message:
+ *                                            type: string
+ *                                            description: Error message
+ *                                            example: Error accessing database
+ *                                        details:
+ *                                            type: string
+ *                                            description: Additional details about error
+ *                                            example: Error getting meals from database
+ */
 router.get("/", requireAuthedUser, getMeals);
 
 module.exports = router;
