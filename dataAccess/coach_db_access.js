99eafca1d3ee48b132a8d1fe910044ffeefedef7
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2023-12-12 13:48:55 -0500

    fix coach_db_access.js

--- dataAccess/coach_db_access.js
+++ dataAccess/coach_db_access.js
@@ -1,57 +1,56 @@
 const { createConnection } = require("../sql_config/database.js");
 const connection = createConnection();
 
-async function getCoachByID_DB(coachID) {
+async function getCoachsByID_DB(coachID) {
   const query =
-    "SELECT c.CoachID, u.FirstName, u.LastName, u.City, u.State, c.Specialties FROM Coach c JOIN User u ON c.UserID = u.UserID WHERE c.coachID = ?";
-  const res = await connection.promise().query(query, [coachID]);
-  return res[0][0];
+    "SELECT c.coachID, u.firstName, u.lastName, u.city, u.state, c.specialties FROM Coach c JOIN User u ON c.userID = u.userID WHERE c.coachID = ?";
+  const [rows, _] = await connection.promise().query(query, [coachID]);
+  return rows;
 }
 
 async function getAllCoaches_DB() {
   const query =
-    "SELECT c.CoachID, u.firstName, u.lastName FROM Coach c INNER JOIN User u WHERE u.UserID = c.CoachID GROUP BY c.CoachID ORDER BY c.CoachID";
+    "SELECT c.coachID, u.firstName, u.lastName FROM Coach c INNER JOIN User u WHERE u.userID = c.coachID GROUP BY c.coachID ORDER BY c.coachID";
   const res = await connection.promise().query(query); //res[0]=rows, res[1]=fields
   return res[0];
 }
 
 async function getSpecializations_DB() {
-  const query =
-    `SELECT DISTINCT specialties FROM Coach`;
+  const query = `SELECT DISTINCT specialties FROM Coach`;
   const res = await connection.promise().query(query); //res[0]=rows, res[1]=fields
   return res[0];
 }
 
+async function searchCoachByName_DB(name) {
+  const query = `SELECT c.coachID, u.firstName, u.lastName 
+      FROM Coach c INNER JOIN User u ON u.userID = c.coachID 
+      WHERE CONCAT(u.firstName, ' ', u.lastName) LIKE '%${name}%' 
+      GROUP BY c.coachID ORDER BY c.coachID`;
+  const res = await connection.promise().query(query); //res[0]=rows, res[1]=fields
+  return res[0];
+}
+
+async function getUsersOfCoach_DB(coachID) {
+  const query =
+    "SELECT userID, firstName, lastName from User WHERE coachID = ?";
+  const res = await connection.promise().query(query, [coachID]);
+  return res[0];
+}
 
 async function getCities_DB() {
   const query =
-    `SELECT State, GROUP_CONCAT(DISTINCT city SEPARATOR ', ') AS Cities
+    `SELECT state, GROUP_CONCAT(DISTINCT city SEPARATOR ', ') AS cities
     FROM User WHERE role = 'coach'
     GROUP BY state ORDER BY state ASC`;
   const res = await connection.promise().query(query); //res[0]=rows, res[1]=fields
   return res[0];
 }
 
-async function searchByName_DB(name) {
-  const query = `SELECT c.CoachID, u.firstName, u.lastName 
-      FROM Coach c INNER JOIN User u ON u.UserID = c.CoachID 
-      WHERE CONCAT(u.firstName, ' ', u.lastName) LIKE '%${name}%' 
-      GROUP BY c.CoachID ORDER BY c.CoachID`;
-  const res = await connection.promise().query(query); //res[0]=rows, res[1]=fields
-  return res[0];
-}
-
-async function getClientsOfCoach_DB(coachID) {
-  const query =  "SELECT UserID, FirstName, LastName from User WHERE CoachID = ?"
-  const res = await connection.promise().query(query,[coachID]);
-  return res[0];
-}
-
 module.exports = {
-  getCoachByID_DB,
+  getCoachsByID_DB,
   getAllCoaches_DB,
   getCities_DB,
   getSpecializations_DB,
-  searchByName_DB,
-  getClientsOfCoach_DB,
+  searchCoachByName_DB,
+  getUsersOfCoach_DB,
 };
