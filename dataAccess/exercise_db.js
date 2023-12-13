0bd94a6809afbd2c4452449c08cd34930ba4fced
Julian-Hame <jhamehs@gmail.com>
2023-12-13 18:58:20 -0500

    Added endpoint to search exercises by muscle group and equipment

---
+++ dataAccess/exercise_db.js
@@ -0,0 +1,21 @@
+const { createConnection } = require("../sql_config/database.js");
+const connection = createConnection();
+
+async function getAllExercises_DB() {
+  const query =
+    `SELECT exerciseID, name, type, difficulty, muscleGroup, equipment FROM Exercise`;
+  const [rows, _] = await connection.promise().query(query); //res[0]=rows, res[1]=fields
+  return rows;
+}
+
+async function searchExercise_DB(muscleGroup, equipment) {
+  const query =
+  `SELECT exerciseID, name, type, difficulty, muscleGroup, equipment 
+  FROM Exercise 
+  WHERE (muscleGroup LIKE '%${muscleGroup}%') AND 
+    (equipment LIKE '%${equipment}%')`;
+  const [rows, _] = await connection.promise().query(query); //res[0]=rows, res[1]=fields
+  return rows;
+}
+
+module.exports = { getAllExercises_DB, searchExercise_DB };
