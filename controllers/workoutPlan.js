dd42f15597ed9a571219ce7ed1c14af624785ea2
Rushi <rrpatel@gmail.com>
2023-12-15 10:18:20 -0500

    added metric and equipment to response for get workoutplan

--- controllers/workoutPlan.js
+++ controllers/workoutPlan.js
@@ -7,48 +7,39 @@ const { getWorkoutPlan_DB, addExercise_DB } = require(
 
 // ONLY FOR USER TO GET THEIR ASSIGNED WORKOUTPLAN
 async function getAssignedWorkoutPlan(req, res) {
-  // makes it so we dont have duplicate code due to where the userID is stored i.e. req.params.userID or req.userID
-  let userID = req.userID;
-
-  // check if userID supplied in path parameters
-  // if userID in path then coach is trying to access their clients workout plan, COACH is logged in
-  // if(req.params.userID != null){
-  //     // checking if supplied userID is the ID of one of the coach' clients
-  //     const coachData = await getCoachFromUserID(req.userID);
-  //     const coachID = coachData.coachID;
-
-  //     let clients = await getUsersOfCoach_DB(coachID);
-  //     clients = clients.filter((client) => {
-  //         return client.userID == req.params.userID});
-
-  //     if(clients.length == 0){
-  //         res.status(403);
-  //         return res.send({
-  //             error: {
-  //                 status: 403,
-  //                 message: `ClientID ${req.params.userID} is not one of this coach's clients.`
-  //             }
-  //         })
-  //     }
-  //     userID = req.params.userID;
-  // }
-
   try {
-    const workoutPlan = await getWorkoutPlan_DB(userID);
-    console.log(workoutPlan);
-    let workoutPlanFormatted = {};
-    workoutPlan.forEach((element) => {
-      if (!(element.dayOfWeek.toLowerCase() in workoutPlanFormatted)) {
-        workoutPlanFormatted[element.dayOfWeek.toLowerCase()] = [];
-      }
-      workoutPlanFormatted[element.dayOfWeek.toLowerCase()].push({
-        exercise: element.name,
-        // temp data until DB changes are made
-        sets: 3,
-        reps: [8, 10, 8],
-        weight: 120,
-      });
-    });
+    const workoutPlan = await getWorkoutPlan_DB(req.userID);
+        let workoutPlanFormatted = {}
+        workoutPlan.forEach(element => {
+          element.dayOfWeek = element.dayOfWeek.toLowerCase();
+          if(!(element.dayOfWeek in workoutPlanFormatted)){
+            workoutPlanFormatted[element.dayOfWeek] = []
+            workoutPlanFormatted[element.dayOfWeek].push({
+              "exercise": element.name,
+              sets: element.sets,
+              reps: [element.reps],
+              weight: element.weight,
+              metric: element.metric,
+              equipment: element.equipment
+            })
+          }else{
+            const existingExercise = workoutPlanFormatted[element.dayOfWeek].find(exercise => exercise.exercise === element.name);
+
+            if (!existingExercise) {
+              workoutPlanFormatted[element.dayOfWeek].push({
+                "exercise": element.name,
+                sets: element.sets,
+                reps: [element.reps],
+                weight: element.weight,
+                metric: element.metric,
+                equipment: element.equipment
+              });
+            } else {
+              existingExercise.reps.push(element.reps);
+            }
+          }
+
+        });
 
     res.status(200);
     return res.send(workoutPlanFormatted);
